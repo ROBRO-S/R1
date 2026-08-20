c3612c524cdd070231e1bd442f1e422d3d0914e5
ismail_tez <ismail_tez@Tezis.laptop>
2026-08-20 17:13:53 +0300

    adding rocks and скалы блять

--- src/server/scripts/Main.server.lua
+++ src/server/scripts/Main.server.lua
@@ -1,31 +1,26 @@
 print("🚀 Сервер R1 запускается...")
 
--- Подключаем модули
 local SSS = game:GetService("ServerScriptService")
 local TerrainGen = require(SSS.TerrainGenerator)
 local ObjectGen = require(SSS.ObjectGenerator)
 
--- Проверяем флаг мира
 local WorldFlag = workspace:FindFirstChild("WorldGenerated_v1")
 
 if not WorldFlag then
     warn("⚠️ Мир не найден. Начинаю полную генерацию...")
     
-    -- 1. Очищаем старые объекты (если они остались с прошлых тестов)
     print(" Очищаю карту от старых объектов...")
     for _, obj in ipairs(workspace:GetChildren()) do
-        if obj.Name == "BigTree" or obj.Name == "thin_tree" or obj.Name == "Stick" then
+        -- Добавил SmallRock и MediumRock в список очистки
+        if obj.Name == "BigTree" or obj.Name == "thin_tree" or obj.Name == "Stick" 
+           or obj.Name == "SmallRock" or obj.Name == "MediumRock" or obj.Name == "MegaTree" then
             obj:Destroy()
         end
     end
     
-    -- 2. Генерируем ландшафт
     TerrainGen.Generate()
-    
-    -- 3. Генерируем деревья и ветки
     ObjectGen.Generate()
     
-    -- 4. Ставим флаг
     local flag = Instance.new("BoolValue")
     flag.Name = "WorldGenerated_v1"
     flag.Parent = workspace
